4f76967a45328d9667d7ed9a5621ca490292074c
Ethan Seiber <ethcseib@ut.utm.edu>
2019-03-06 20:42:59 -0600

    Character movement

--- BurningChoices code/BurningChoices code/MainWindow.xaml.cs
+++ BurningChoices code/BurningChoices code/MainWindow.xaml.cs
@@ -24,5 +24,47 @@ namespace BurningChoices_code
         {
             InitializeComponent();
         }
+
+        private void CharacterInitialized(object sender, EventArgs e)
+        {
+            /*<Summary> establishes the main character model. bitmap.UriSource will have to change depending on where the images are located on the pc. Could change it to "mlg.jpg" if the image
+             is moved to the .exe folder </Summary>*/
+
+            BitmapImage bitmap = new BitmapImage();//system.Windows.Media.BitmapImage rather than Sytem.Drawing
+
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(@"C:\Users\Ethan Seiber\Documents\1 School\1 Programming\csci352\Burnining Choices\BurningChoices\Image Assets\mlg.jpg", UriKind.RelativeOrAbsolute);
+            bitmap.EndInit();
+
+            character.Source = bitmap;
+        }
+
+        private void TraverseCanvas_KeyDown(object sender, KeyEventArgs e)//controls the movement of the character
+        {
+            if (e.Key == Key.W)
+            {
+                Canvas.SetTop(character, Canvas.GetTop(character) - 10);
+            }
+            if (e.Key == Key.S)
+            {
+                Canvas.SetTop(character, Canvas.GetTop(character) + 10);
+            }
+            else if (e.Key == Key.A)
+            {
+                Canvas.SetLeft(character, Canvas.GetLeft(character) - 10);
+            }
+            else if (e.Key == Key.D)
+            {
+                Canvas.SetLeft(character, Canvas.GetLeft(character) + 10);
+            }
+
+        }
+
+        private void CanvasInitialized(object sender, EventArgs e)//allows the canvas to take input which allows things like image movement
+        {
+            canvas.Focusable = true;
+            canvas.Focus();
+        }
     }
 }
+/*plan: take the grid creating many tiny cells and then traverse over them to simulate movement*/
